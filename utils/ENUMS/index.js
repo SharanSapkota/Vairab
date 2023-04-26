106ac8c6d60a7060e884d8cfd1997ef8a63811f6
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-26 11:20:07 +0545

    Add enums

--- utils/ENUMS/index.js
+++ utils/ENUMS/index.js
@@ -1,1 +1,4 @@
-export const NOT_DEFINED = 'Not Defined'
+const NOT_DEFINED = 'Not Defined'
+const LOG_FORMAT_REGEX = /^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+) (\S+)" (\d{3}) (\S+) "(\S+)" "([^"]+)"$/
+
+module.exports = { NOT_DEFINED, LOG_FORMAT_REGEX }
