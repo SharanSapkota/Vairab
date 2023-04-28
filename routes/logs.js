58d69f81904875f356d9bbbf3ef714bcdf63d825
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-28 19:33:01 +0545

    Add authorization middleware to apis

--- routes/logs.js
+++ routes/logs.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { postApacheLogs, getLogs, postNgInxLogs, getApacheLogs, getNgInxLogs } = require('../controller/LogRecord');
+const { auth } = require('../middleware/auth');
 
 const router   = express.Router();
 
-router.post('/apacheLogs', postApacheLogs);
+router.post('/apacheLogs',  postApacheLogs);
 router.post('/nginxLogs', postNgInxLogs);
-router.get('/getApacheLogs', getApacheLogs)
+router.get('/getApacheLogs', auth, getApacheLogs)
 router.get('/getNgInxLogs', getNgInxLogs)
 
 
-router.get('/getAllLogs', getLogs);
+router.get('/getAllLogs',auth, getLogs);
 
 module.exports = router;
