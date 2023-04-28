aca4a44c83e249edf81498ee1c02c4730a67db4b
SharanSapkota <sapkotarambbo@gmail.com>
2023-04-28 22:05:23 +0545

    Add try catch to auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,15 @@
 const jwt = require('jsonwebtoken') 
  
  const auth = ({ headers }, res, next) => {
-    const userData = jwt.verify(headers.authorization, process.env.JWT_SECRET)
-    if(!userData) {
-        res.status(501).json({message: "Unauthorised"})
+    try{
+        const userData = jwt.verify(headers.authorization, process.env.JWT_SECRET)
+        if(!userData) {
+            res.status(501).json({message: "Unauthorised"})
+        }
+        next();
+    } catch(e) {
+        res.status(404).json({message: "Something went wrong!"})
     }
-    next();
 }
 
 module.exports = {auth}
