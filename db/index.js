9b2df9978ac4c29c8a13418071c7061a719d8dcf
SharanSapkota <sapkotarambbo@gmail.com>
2023-05-04 12:50:24 +0545

    Take mongo URI from env

--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
-const mongoose   = require('mongoose');
-const ur = "mongodb+srv://vairab:vairab123@cluster0.rdbbh.mongodb.net/test";
-const connectToMongo = async () => {
-try {
-    mongoose.set('strictQuery', false)
-    mongoose.connect(ur) 
-    console.log('Mongo connected')
-}
-catch(error) {
-    console.error(error)
-}
-}
+    const mongoose   = require('mongoose');
+    const ur = process.env.MONGO_URI;
+    const connectToMongo = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        mongoose.connect(ur) 
+        console.log('Mongo connected')
+    }
+    catch(error) {
+        console.error(error)
+    }
+    }
 
-module.exports = {connectToMongo}
+    module.exports = {connectToMongo}
